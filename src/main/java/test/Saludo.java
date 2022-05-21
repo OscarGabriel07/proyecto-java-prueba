77b3b9fd86106c884a827fbd5dbed470ccd2df2c
Oscar Farfan <oscarfarfan92@gmail.com>
2022-05-21 12:02:54 -0500

    Se agrega signo de exclamación al principio

--- src/main/java/test/Saludo.java
+++ src/main/java/test/Saludo.java
@@ -2,6 +2,6 @@ package test;
 
 public class Saludo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+        System.out.println("¡Hola Mundo!");
     }
 }
